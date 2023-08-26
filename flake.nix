15135945891e742e2b1fb81827403d2bf0e703e3
Akira Komamura <akira.komamura@gmail.com>
2023-08-26 17:49:20 +0900

    style: Don't pass default arguments to makeOverridable as argument

--- flake.nix
+++ flake.nix
@@ -10,69 +10,69 @@
   } @ inputs: let
     inherit (nixpkgs) lib;
     eachSystem = nixpkgs.lib.genAttrs (import systems);
+
+    defaultCommitCallback =
+      # Reset the committer to prevent adding the rewriter's identity.
+      # Provided by Иван Жеков at
+      # https://github.com/newren/git-filter-repo/issues/379#issuecomment-1182480579
+      ''
+        commit.committer_name = commit.author_name
+        commit.committer_email = commit.author_email
+        commit.committer_date = commit.author_date
+      '';
   in {
     packages = eachSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      default =
-        lib.makeOverridable (
-          {commitCallback}:
-            pkgs.writeShellApplication {
-              name = "default";
-              runtimeInputs = [
-                pkgs.git-filter-repo
-              ];
-              text = ''
-                usage() {
-                  echo "Usage: default <upstream ref>" >&2
-                }
+      default = lib.makeOverridable (
+        {commitCallback ? defaultCommitCallback}:
+          pkgs.writeShellApplication {
+            name = "default";
+            runtimeInputs = [
+              pkgs.git-filter-repo
+            ];
+            text = ''
+              usage() {
+                echo "Usage: default <upstream ref>" >&2
+              }
 
-                if [[ $# -eq 0 ]]; then
+              if [[ $# -eq 0 ]]; then
+                usage
+                exit 1
+              fi
+
+              case "$1" in
+                -h|--help)
                   usage
-                  exit 1
-                fi
+                  exit
+                   ;;
+                *)
+                  upstream="$1" ;;
+              esac
 
-                case "$1" in
-                  -h|--help)
-                    usage
-                    exit
-                     ;;
-                  *)
-                    upstream="$1" ;;
-                esac
+              start="$(git rev-parse HEAD)"
+              branch="$(git symbolic-ref --short HEAD)"
+              tmp="recipes-$(date +%s)"
+              base=$(git merge-base "$upstream" "$start")
 
-                start="$(git rev-parse HEAD)"
-                branch="$(git symbolic-ref --short HEAD)"
-                tmp="recipes-$(date +%s)"
-                base=$(git merge-base "$upstream" "$start")
+              git switch -c "$tmp" "$upstream"
 
-                git switch -c "$tmp" "$upstream"
+              cleanup() {
+                git branch -D "$tmp"
+              }
 
-                cleanup() {
-                  git branch -D "$tmp"
-                }
+              trap cleanup ERR EXIT
 
-                trap cleanup ERR EXIT
-
-                git filter-repo --path recipes/ --refs "$base..HEAD" ${
-                  lib.optionalString (commitCallback != null)
-                  ("--commit-callback '\n" + commitCallback + "'")
-                }
-                git rebase "$start"
-                git switch "$branch"
-                git merge --ff-only "$tmp"
-              '';
-            }
-        ) {
-          # Reset the committer to prevent adding the rewriter's identity.
-          # Provided by Иван Жеков at
-          # https://github.com/newren/git-filter-repo/issues/379#issuecomment-1182480579
-          commitCallback = ''
-            commit.committer_name = commit.author_name
-            commit.committer_email = commit.author_email
-            commit.committer_date = commit.author_date
-          '';
-        };
+              git filter-repo --path recipes/ --refs "$base..HEAD" ${
+                lib.optionalString (commitCallback != null)
+                ("--commit-callback '\n" + commitCallback + "'")
+              }
+              git rebase "$start"
+              git switch "$branch"
+              git merge --ff-only "$tmp"
+            '';
+          }
+      ) {};
     });
   };
 }
